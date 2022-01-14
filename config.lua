35a7f57612b4457d0fc57e4ff7ec71f4eb74801a
falcon1641 <94806141+falcon1641@users.noreply.github.com>
2022-01-14 10:45:56 -0500

    add jobs, change locations

--- config.lua
+++ config.lua
@@ -5,16 +5,20 @@ Config.WipeInventoryOnRespawn = true
 
 Config.Locations = {
     ["checking"] = {
-	    [1] = vector3(308.19, -595.35, 43.29),
-	    [2] = vector3(-254.54, 6331.78, 32.43), -- paleto
+	[1] = vector3(308.19, -595.35, 43.29),
+	[2] = vector3(-254.54, 6331.78, 32.43), -- paleto
+        [3] = vector3(1830.72, 3681.22, 34.27),
     },
     ["duty"] = {
         [1] = vector3(311.18, -599.25, 43.29),
         [2] = vector3(-254.88, 6324.5, 32.58),
     },
     ["vehicle"] = {
-        [1] = vector4(294.578, -574.761, 43.179, 35.792),
+        [1] = vector4(289.3, -610.49, 43.37, 65.07),
         [2] = vector4(-234.28, 6329.16, 32.15, 222.5),
+        [3] = vector4(-1673.56, 50.59, 63.34, 136.24),
+        [4] = vector4(-1669.78, 46.93, 63.34, 137.39),
+        [5] = vector4(-1666.36, 43.45, 63.36, 125.37),
     },
     ["helicopter"] = {
         [1] = vector4(351.58, -587.45, 74.16, 160.5),
@@ -23,6 +27,7 @@ Config.Locations = {
     ["armory"] = {
         [1] = vector3(309.93, -602.94, 43.29),
         [2] = vector3(-245.13, 6315.71, 32.82),
+        [3] = vector3(-1674.44, 53.58, 63.34)
     },
     ["roof"] = {
         [1] = vector4(338.5, -583.85, 74.16, 245.5),
@@ -43,36 +48,93 @@ Config.Locations = {
         [7] = {coords = vector4(349.82, -583.33, 43.02, 152.08), taken = false, model = -1091386327},
         [8] = {coords = vector4(326.98, -576.17, 43.02, 152.08), taken = false, model = -1091386327},
 	--- paleto
-	    [9] = {coords = vector4(-252.43, 6312.25, 32.34, 313.48), taken = false, model = 2117668672},
+	[9] = {coords = vector4(-252.43, 6312.25, 32.34, 313.48), taken = false, model = 2117668672},
         [10] = {coords = vector4(-247.04, 6317.95, 32.34, 134.64), taken = false, model = 2117668672},
         [11] = {coords = vector4(-255.98, 6315.67, 32.34, 313.91), taken = false, model = 2117668672},
     },
+	--sandy
+        [12] = {coords = vector4(1825.69, 3678.57, 34.18, 287.22), taken = false, model = 1631638868},
+        [13] = {coords = vector4(1829.76, 3676.03, 34.18, 359.3), taken = false, model = 1631638868},
+    },
     ["stations"] = {
-        [1] = {label = "Pillbox Hospital", coords = vector4(304.27, -600.33, 43.28, 272.249)}
+        [1] = {label = "Pillbox Hospital", coords = vector4(304.27, -600.33, 43.28, 272.249)},
+        [2] = {label = "Sandy Medical Center", coords = vector4(1829.2, 3682.51, 34.27, 40.48)}
     }
 }
 
-Config.AuthorizedVehicles = {
+Config.AuthorizedVehicles ={
 	-- Grade 0
 	[0] = {
-		["ambulance"] = "Ambulance",
+	["16ramambo"] = "SAFR Ambulance",
+        ["prescue"] = "SAFR Rescue",
+        ["pengine"] = "SAFR Engine",
+        ["pladder"] = "SAFR Ladder",
+
 	},
 	-- Grade 1
 	[1] = {
-		["ambulance"] = "Ambulance",
+        ["16ramambo"] = "SAFR Ambulance",
+        ["prescue"] = "SAFR Rescue",
+        ["pengine"] = "SAFR Engine",
+        ["pladder"] = "SAFR Ladder",
 
 	},
 	-- Grade 2
 	[2] = {
-		["ambulance"] = "Ambulance",
+	["16ramambo"] = "SAFR Ambulance",
+        ["prescue"] = "SAFR Rescue",
+        ["pengine"] = "SAFR Engine",
+        ["pladder"] = "SAFR Ladder",
 	},
 	-- Grade 3
 	[3] = {
-		["ambulance"] = "Ambulance",
+        ["16ramambo"] = "SAFR Ambulance",
+        ["prescue"] = "SAFR Rescue",
+        ["pengine"] = "SAFR Engine",
+        ["pladder"] = "SAFR Ladder",
 	},
 	-- Grade 4
 	[4] = {
-		["ambulance"] = "Ambulance",
+	["16ramambo"] = "SAFR Ambulance",
+        ["prescue"] = "SAFR Rescue",
+        ["pengine"] = "SAFR Engine",
+        ["pladder"] = "SAFR Ladder",
+	},
+    [5] = {
+	["16ramambo"] = "SAFR Ambulance",
+        ["prescue"] = "SAFR Rescue",
+        ["pengine"] = "SAFR Engine",
+        ["pladder"] = "SAFR Ladder",
+	},
+    [6] = {
+	["16ramambo"] = "SAFR Ambulance",
+        ["prescue"] = "SAFR Rescue",
+        ["pengine"] = "SAFR Engine",
+        ["pladder"] = "SAFR Ladder",
+	},
+    [7] = {
+	["16ramambo"] = "SAFR Ambulance",
+        ["prescue"] = "SAFR Rescue",
+        ["pengine"] = "SAFR Engine",
+        ["pladder"] = "SAFR Ladder",
+	},
+    [8] = {
+	["16ramambo"] = "SAFR Ambulance",
+        ["prescue"] = "SAFR Rescue",
+        ["pengine"] = "SAFR Engine",
+        ["pladder"] = "SAFR Ladder",
+	},
+    [9] = {
+	["16ramambo"] = "SAFR Ambulance",
+        ["prescue"] = "SAFR Rescue",
+        ["pengine"] = "SAFR Engine",
+        ["pladder"] = "SAFR Ladder",
+	},
+    [10] = {
+	["16ramambo"] = "SAFR Ambulance",
+        ["prescue"] = "SAFR Rescue",
+        ["pengine"] = "SAFR Engine",
+        ["pladder"] = "SAFR Ladder",
 	}
 }
 
@@ -133,7 +195,7 @@ Config.Items = {
     }
 }
 
-Config.BillCost = 2000
+Config.BillCost = 500
 Config.DeathTime = 300
 Config.CheckTime = 10
 
@@ -176,7 +238,7 @@ Config.AIHealTimer = 20
 --[[
     BleedTickRate : How much time, in seconds, between bleed ticks
 ]]
-Config.BleedTickRate = 30
+Config.BleedTickRate = 20
 
 --[[
     BleedMovementTick : How many seconds is taken away from the bleed tick rate if the player is walking, jogging, or sprinting
